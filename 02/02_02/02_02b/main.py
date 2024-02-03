86bcccec76bbf8eeaf9033bc077ade7facd80229
kaveto <kaveto1110@live.com>
2024-02-03 23:47:44 +0000

    changes

--- 02/02_02/02_02b/main.py
+++ 02/02_02/02_02b/main.py
@@ -2,3 +2,9 @@
 Problem Statement: Compute the average number of pets each student 
 has in a given class. 
 '''
+student_pet_count_list=[0,1,2,5,8]
+
+numStudent = len(student_pet_count_list)
+print(numStudent)
+
+#average
