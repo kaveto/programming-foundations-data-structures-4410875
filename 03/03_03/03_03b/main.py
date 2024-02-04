1f2e7dc9a8bfbe2db1b12b0d0ff7a2a8fc0554f8
kaveto <kaveto1110@live.com>
2024-02-04 01:16:28 +0000

    changes in 3 chapter

--- 03/03_03/03_03b/main.py
+++ 03/03_03/03_03b/main.py
@@ -7,3 +7,16 @@ user_preferences = {
     "volume_level": 80,
     "date_format": "MM/DD/YYYY"
 }
+
+user_preferences["language"] = "Spanish" #overwride the value
+user_preferences["volume_level"] = 50
+user_preferences["highlight_color"] = "yellow" #add new value
+
+#to delete a value without retrive the item
+del user_preferences["currency"]
+
+#to delete a value retriving the item
+#"N/A" prevent an error if the key doesn't exist in the dictionary
+removed_item = user_preferences.pop("date_format","N/A") 
+
+print(user_preferences)
--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -8,7 +8,13 @@ user_preferences = {
 
 
 def update_preferences(user_pref):
-    return {}
+#this line optimize the execution time}
+    return {key:value for key, value in user_pref.items() if value is not None}
+#    updated_preferencs = {}
+#    for key,value in user_pref.items():
+#        if value is not None:
+#            updated_preferencs[key] = value
+#    return updated_preferencs
 
 
 print(update_preferences(user_preferences))
