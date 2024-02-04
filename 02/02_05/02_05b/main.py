421a199b8776338e620b74758bd67d21501448bb
kaveto <kaveto1110@live.com>
2024-02-04 00:46:04 +0000

    edit chapter 2

--- 02/02_05/02_05b/main.py
+++ 02/02_05/02_05b/main.py
@@ -4,3 +4,10 @@ seating_chart = [
     ["Katherine", "Lauren", "Mary", "Nathan", "Olive"],
     ["Chad", "April", "Matt", "Thomas", "Penny"]
 ]
+#get the value "lauren". from the most to least
+#print(seating_chart[2][1]) 
+
+#recorriendo una matriz
+for i,row in enumerate(seating_chart):
+  for j, student_name in enumerate(row):
+    print(f"student {student_name} is in {i+1}, seat{j+1}")
--- 02/02_06/02_06b/main.py
+++ 02/02_06/02_06b/main.py
@@ -1,1 +1,13 @@
 # Tuples are immutable array-like structures
+point = (5,2)
+x = point[0]
+y = point[1]
+
+def calculate_square_properties(side_lenght):
+  area = side_lenght ** 2
+  perimeter = 4 * side_lenght
+  return (area, perimeter)
+
+result = calculate_square_properties(5)
+print("area: ", result[0])
+print("perimeter: ", result[1])
